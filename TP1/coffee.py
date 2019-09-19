9bd1e436f3d1025cdb577ac422cf1ba3fffc3f6b
David-LETINAUD <david.letinaud@gmail.com>
2019-09-19 12:36:05 -0400

    coffee

---
+++ TP1/coffee.py
@@ -0,0 +1,8 @@
+import cozmo
+
+def coffee(robot: cozmo.robot.Robot):
+    robot.say_text("Hy need coffee!", True, in_parallel=True, duration_scalar=2,use_cozmo_voice=True).wait_for_completed()
+
+
+
+cozmo.run_program(coffee)

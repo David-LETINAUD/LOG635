9f46936e68ce79ada76566d40025fc8b2a611793
camilletr21 <51219843+camilletr21@users.noreply.github.com>
2019-09-24 19:14:02 -0400

    Add files via upload

--- TP1/elephant.py
+++ TP1/elephant.py
@@ -1,9 +1,9 @@
 import cozmo
 
 
-def roll(robot: cozmo.robot.Robot):
+def elephant(robot: cozmo.robot.Robot):
     robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabElephant).wait_for_completed()
 
 
-#cozmo.run_program(reveil)
+#cozmo.run_program(elephant)
   

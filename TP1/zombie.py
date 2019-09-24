387db383e730f458b54dbf9ce462ec1e2503c96b
camilletr21 <51219843+camilletr21@users.noreply.github.com>
2019-09-24 19:58:42 -0400

    Add files via upload

---
+++ TP1/zombie.py
@@ -0,0 +1,10 @@
+import cozmo
+
+
+def zombie(robot: cozmo.robot.Robot):
+    
+    robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabZombie, ignore_body_track=True).wait_for_completed()
+ 
+
+#cozmo.run_program(zombie)
+

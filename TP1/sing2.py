19229232de0656d4ac230024e192b8aaf85f8872
Arès <jean-sebastien.ares.1@ens.etsmtl.ca>
2019-09-24 18:27:08 -0400

    sing2

---
+++ TP1/sing2.py
@@ -0,0 +1,8 @@
+import cozmo
+
+
+def singWholeNote(robot: cozmo.robot.Robot):
+    
+    song.SongNote(noteType=_NoteType(name='C2', id=0), noteDuration=_NoteDuration(name='Whole', id=0)).wait_for_completed()
+
+#cozmo.run_program(nap)

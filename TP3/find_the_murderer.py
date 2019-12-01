41015305b99048f6ad754854768059fa19563e62
David-LETINAUD <david.letinaud@gmail.com>
2019-12-01 10:17:58 -0500

    new repo

--- TP3/find_the_murderer.py
+++ TP3/find_the_murderer.py
@@ -2,7 +2,6 @@ import cozmo
 import time
 from cozmo.util import degrees,Pose
 from inference_class import *
-from Map import create_walls, position, create_positions,launch_all
 
 cube_taps = 0
 
@@ -11,13 +10,9 @@ def handle_object_tapped(evt, **kw):
     #print(evt.obj.object_id)
     # This will be called whenever an EvtObjectMovingStarted event is dispatched -
     # whenever we detect a cube starts moving (via an accelerometer in the cube)
-    if evt.obj.object_id ==1:
+    if evt.obj.object_id ==2:
         cube_taps = cube_taps + evt.tap_count if cube_taps<2 else 0
         print(cube_taps)
-
-# Parcours fixe mais objet diff dans chaque 
-# trouve un mqrqueyr => quel est l'objet 
-# trouve le cube victime => demande qui c'est 
 
 agent = CrimeInference()
 
@@ -28,80 +23,6 @@ for a_p in alive_people:
 
 def Filter(string, substr): 
     return [s for s in string if s not in substr]
-
-def reaction_piece_1(robot: cozmo.robot.Robot):
-    # On se rend compte que Scarlet est morte par étranglement
-    fact = ['Scarlet a des marques au cou']
-    agent.add_clause(to_fol(fact, 'grammars/personne_marque.fcfg'))
-    fact = ['Scarlet est dans le bureau']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece.fcfg'))
-
-    # On sait que Peacock est dans le bureau
-    fact = ['Peacock est dans le bureau']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece.fcfg'))
-
-    # Demande à Peacock l'heure du decès -> Rep : 14h
-    robot.say_text("A quelle heure est morte {}".format(agent.get_victim()), True, in_parallel=True, duration_scalar=0.5,use_cozmo_voice=True).wait_for_completed() 
-    fact = input("Entrez l\'heure")
-    #fact = ['Scarlet est morte à 14h']
-    agent.add_clause(to_fol(fact, 'grammars/personne_morte_heure.fcfg'))
-
-    uneHeureApres = agent.get_crime_hour() + 1
-
-    agent.add_clause('UneHeureApresCrime({})'.format(uneHeureApres))
-
-    # Demande à Peacock dans quelle pièce il était une heure après le meurtre -> Rep : Peacock dans le Salon à 15h
-    fact = ['Peacock était dans le salon à ' + str(uneHeureApres) + 'h']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece_heure.fcfg'))
-
-def reaction_piece_2(robot: cozmo.robot.Robot):
-    # Dans le salon
-    # Voit qu'il y a un fusil et Plum dans le salon
-    fact = ['Le fusil est dans le salon']
-    agent.add_clause(to_fol(fact, 'grammars/arme_piece.fcfg'))
-    fact = ['Plum est dans le salon']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece.fcfg'))
-
-    uneHeureApres = agent.get_crime_hour() + 1
-
-    # Demande à Plum dans quelle pièce il était une heure après le meurtre -> Rep : Plum dans le Salon à 15h
-    fact = ['Plum était dans le salon à ' + str(uneHeureApres) + 'h']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece_heure.fcfg'))
-
-def reaction_piece_3(robot: cozmo.robot.Robot):
-    # Dans la cuisine
-    # Voit qu'il y a un couteau, White et Mustard dans la cuisine
-    fact = ['Le couteau est dans la cuisine']
-    agent.add_clause(to_fol(fact, 'grammars/arme_piece.fcfg'))
-    fact = ['White est dans la cuisine']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece.fcfg'))
-    fact = ['Mustard est dans la cuisine']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece.fcfg'))
-
-    uneHeureApres = agent.get_crime_hour() + 1
-
-    # Demande à White dans quelle pièce il était une heure après le meurtre -> Rep : White dans la Cuisine à 15h
-    fact = ['White était dans la cuisine à ' + str(uneHeureApres) + 'h']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece_heure.fcfg'))
-
-    # Demande à Mustard dans quelle pièce il était une heure après le meurtre -> Rep : Mustard dans le Garage à 15h
-    fact = ['Mustard était dans le garage à ' + str(uneHeureApres) + 'h']
-    agent.add_clause(to_fol(fact, 'grammars/personne_piece_heure.fcfg'))
-
-def reaction_piece_4(robot: cozmo.robot.Robot):
-    # Dans le garage
-    # On se rend compte qu'il y a une corde dans le garage
-    fact = ['La corde est dans le garage']
-    agent.add_clause(to_fol(fact, 'grammars/arme_piece.fcfg'))
-
-
-
-# function_tab = []
-# function_tab.append(reaction_piece_1)
-# function_tab.append(reaction_piece_2)
-# function_tab.append(reaction_piece_3)
-# function_tab.append(reaction_piece_4)
-
 
 def analyse_victime(robot):
     global cube_taps
@@ -215,7 +136,6 @@ def indice_3(robot):
 
     fact = ['{} était dans le {} à '.format(suspect,piece) + str(hour_plus_one) + 'h']
     agent.add_clause(to_fol(fact, 'grammars/personne_piece_heure.fcfg'))
-    
 
 def Conclusions():
     # Conclusions
@@ -227,10 +147,12 @@ def Conclusions():
     print("Personnes innocentes : ", agent.get_innocent()) 
 
 
-############# COZMO PROGRAM
+############# COZMO PROGRAM FOR test
 def cozmo_program(robot: cozmo.robot.Robot):
     robot.world.delete_all_custom_objects()
     robot.add_event_handler(cozmo.objects.EvtObjectTapped, handle_object_tapped)
+
+    #create_walls(robot)
 
     analyse_victime(robot)
     indice_1(robot)
@@ -238,29 +160,6 @@ def cozmo_program(robot: cozmo.robot.Robot):
     indice_3(robot)
     Conclusions()
 
-    #create_walls(robot)
-
-
-    # position_tab = create_positions(robot,function_tab)
-    # launch_all(position_tab)
-
-    # Conclusions()
-
-    # for s in stop :
-    #     robot.go_to_pose(s, relative_to_robot=False).wait_for_completed()
-    #     robot.say_text("Où suis je ?", True, in_parallel=True, duration_scalar=0.5,use_cozmo_voice=True).wait_for_completed() 
-    #     piece = input("entrez nom de la pièce")
-    #     robot.say_text("je suis à" + piece, True, in_parallel=True, duration_scalar=0.5,use_cozmo_voice=True).wait_for_completed() 
-
-
-
-cozmo.run_program(cozmo_program, use_3d_viewer=True, use_viewer=True)
-
-
-# Associer chaque cube_ID à une fonction (victime/buzzer/meurtrier)
-# Associer une position à chaque une action
-# Pour chaque position
-# 
-#
+#cozmo.run_program(cozmo_program, use_3d_viewer=True, use_viewer=True)
 
 
--- TP3/inference_class.py
+++ TP3/inference_class.py
@@ -17,8 +17,6 @@ def to_fol(fact, grammar):
     print(sent)
     return sent    
 
-
-
 # Permet d'inferer qui est le meurtrier, quand, comment, où il a tué.
 class CrimeInference:
 

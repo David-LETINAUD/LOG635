4a50138006ef7caa5483d47d0f7bed1f7c0dcc99
jsares <jeansebastien.ares@gmail.com>
2019-12-03 18:51:15 -0500

    smaller walls

--- TP3/Map.py
+++ TP3/Map.py
@@ -13,8 +13,8 @@ def create_walls(robot: cozmo.robot.Robot):
     wall2 = Pose(0, 320, 0, angle_z=degrees(0))
     wall2 = robot.world.create_custom_fixed_object(wall2, WALL_WIDTH, 360, WALL_HEIGHT,relative_to_robot=False)
     
-    wall3 = Pose(200, 440, 0, angle_z=degrees(0))
-    wall3 = robot.world.create_custom_fixed_object(wall3, WALL_WIDTH, 120, WALL_HEIGHT,relative_to_robot=False)
+    wall3 = Pose(200, 430, 0, angle_z=degrees(0))
+    wall3 = robot.world.create_custom_fixed_object(wall3, WALL_WIDTH, 100, WALL_HEIGHT,relative_to_robot=False)
 
     # wall4 = Pose(280, 150, 0, angle_z=degrees(0))
     # wall4 = robot.world.create_custom_fixed_object(wall4, WALL_WIDTH, 30, WALL_HEIGHT,relative_to_robot=False)
@@ -26,20 +26,20 @@ def create_walls(robot: cozmo.robot.Robot):
     # wall6 = robot.world.create_custom_fixed_object(wall6, WALL_WIDTH, 60, WALL_HEIGHT,relative_to_robot=False)
     
     # # --- VERTICAL ---
-    wall7 = Pose(50, 140, 0, angle_z=degrees(0))
-    wall7 = robot.world.create_custom_fixed_object(wall7, 100, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
+    wall7 = Pose(40, 140, 0, angle_z=degrees(0))
+    wall7 = robot.world.create_custom_fixed_object(wall7, 80, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
     
-    wall8 = Pose(50, 380, 0, angle_z=degrees(0))
-    wall8 = robot.world.create_custom_fixed_object(wall8, 100, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
+    wall8 = Pose(40, 380, 0, angle_z=degrees(0))
+    wall8 = robot.world.create_custom_fixed_object(wall8, 80, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
 
     wall9 = Pose(190, 500, 0, angle_z=degrees(0))
     wall9 = robot.world.create_custom_fixed_object(wall9, 380, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
 
-    wall10 = Pose(330, 340, 0, angle_z=degrees(0))
-    wall10 = robot.world.create_custom_fixed_object(wall10, 100, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
+    wall10 = Pose(340, 340, 0, angle_z=degrees(0))
+    wall10 = robot.world.create_custom_fixed_object(wall10, 80, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
 
-    wall11 = Pose(330, 180, 0, angle_z=degrees(0))
-    wall11 = robot.world.create_custom_fixed_object(wall11, 100, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
+    wall11 = Pose(340, 180, 0, angle_z=degrees(0))
+    wall11 = robot.world.create_custom_fixed_object(wall11, 80, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
 
     # wall12 = Pose(260, 140, 0, angle_z=degrees(0))
     # wall12 = robot.world.create_custom_fixed_object(wall12, 40, WALL_WIDTH, WALL_HEIGHT,relative_to_robot=False)
